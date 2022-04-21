50af8f95f35ab0d32466bb1ac92cd148e4563565
kallyas <kallyasmedia@gmail.com>
2022-04-21 21:30:17 +0300

    ch: add featured jobs

---
+++ frontend/src/components/featuredJobs/FeaturedJobs.js
@@ -0,0 +1,233 @@
+/* eslint-disable */
+import React from "react";
+
+const FeaturedJobs = () => {
+  return (
+    <section className="job-section">
+      <div className="auto-container">
+        <div className="sec-title text-center">
+          <h2>Featured Jobs</h2>
+          <div className="text">Know your worth and find the job that qualify your life</div>
+        </div>
+
+        <div
+          className="row wow fadeInUp animated"
+          style={{ visibility: "visible", animationName: "fadeInUp" }}
+        >
+          <div className="job-block col-lg-6 col-md-12 col-sm-12">
+            <div className="inner-box">
+              <div className="content">
+                <span className="company-logo">
+                  <img src="images/resource/company-logo/1-1.png" alt="" />
+                </span>
+                <h4>
+                  <a href="#">Software Engineer (Android), Libraries</a>
+                </h4>
+                <ul className="job-info">
+                  <li>
+                    <span className="icon flaticon-briefcase"></span> Segment
+                  </li>
+                  <li>
+                    <span className="icon flaticon-map-locator"></span> London, UK
+                  </li>
+                  <li>
+                    <span className="icon flaticon-clock-3"></span> 11 hours ago
+                  </li>
+                  <li>
+                    <span className="icon flaticon-money"></span> $35k - $45k
+                  </li>
+                </ul>
+                <ul className="job-other-info">
+                  <li className="time">Full Time</li>
+                  <li className="privacy">Private</li>
+                  <li className="required">Urgent</li>
+                </ul>
+                <button className="bookmark-btn">
+                  <span className="flaticon-bookmark"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="job-block col-lg-6 col-md-12 col-sm-12">
+            <div className="inner-box">
+              <div className="content">
+                <span className="company-logo">
+                  <img src="images/resource/company-logo/1-2.png" alt="" />
+                </span>
+                <h4>
+                  <a href="#">Recruiting Coordinator</a>
+                </h4>
+                <ul className="job-info">
+                  <li>
+                    <span className="icon flaticon-briefcase"></span> Segment
+                  </li>
+                  <li>
+                    <span className="icon flaticon-map-locator"></span> London, UK
+                  </li>
+                  <li>
+                    <span className="icon flaticon-clock-3"></span> 11 hours ago
+                  </li>
+                  <li>
+                    <span className="icon flaticon-money"></span> $35k - $45k
+                  </li>
+                </ul>
+                <ul className="job-other-info">
+                  <li className="time">Full Time</li>
+                  <li className="privacy">Private</li>
+                  <li className="required">Urgent</li>
+                </ul>
+                <button className="bookmark-btn">
+                  <span className="flaticon-bookmark"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="job-block col-lg-6 col-md-12 col-sm-12">
+            <div className="inner-box">
+              <div className="content">
+                <span className="company-logo">
+                  <img src="images/resource/company-logo/1-3.png" alt="" />
+                </span>
+                <h4>
+                  <a href="#">Product Manager, Studio</a>
+                </h4>
+                <ul className="job-info">
+                  <li>
+                    <span className="icon flaticon-briefcase"></span> Segment
+                  </li>
+                  <li>
+                    <span className="icon flaticon-map-locator"></span> London, UK
+                  </li>
+                  <li>
+                    <span className="icon flaticon-clock-3"></span> 11 hours ago
+                  </li>
+                  <li>
+                    <span className="icon flaticon-money"></span> $35k - $45k
+                  </li>
+                </ul>
+                <ul className="job-other-info">
+                  <li className="time">Full Time</li>
+                  <li className="privacy">Private</li>
+                  <li className="required">Urgent</li>
+                </ul>
+                <button className="bookmark-btn">
+                  <span className="flaticon-bookmark"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="job-block col-lg-6 col-md-12 col-sm-12">
+            <div className="inner-box">
+              <div className="content">
+                <span className="company-logo">
+                  <img src="images/resource/company-logo/1-4.png" alt="" />
+                </span>
+                <h4>
+                  <a href="#">Senior Product Designer</a>
+                </h4>
+                <ul className="job-info">
+                  <li>
+                    <span className="icon flaticon-briefcase"></span> Segment
+                  </li>
+                  <li>
+                    <span className="icon flaticon-map-locator"></span> London, UK
+                  </li>
+                  <li>
+                    <span className="icon flaticon-clock-3"></span> 11 hours ago
+                  </li>
+                  <li>
+                    <span className="icon flaticon-money"></span> $35k - $45k
+                  </li>
+                </ul>
+                <ul className="job-other-info">
+                  <li className="time">Full Time</li>
+                  <li className="privacy">Private</li>
+                  <li className="required">Urgent</li>
+                </ul>
+                <button className="bookmark-btn">
+                  <span className="flaticon-bookmark"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="job-block col-lg-6 col-md-12 col-sm-12">
+            <div className="inner-box">
+              <div className="content">
+                <span className="company-logo">
+                  <img src="images/resource/company-logo/1-5.png" alt="" />
+                </span>
+                <h4>
+                  <a href="#">Senior Full Stack Engineer, Creator Success</a>
+                </h4>
+                <ul className="job-info">
+                  <li>
+                    <span className="icon flaticon-briefcase"></span> Segment
+                  </li>
+                  <li>
+                    <span className="icon flaticon-map-locator"></span> London, UK
+                  </li>
+                  <li>
+                    <span className="icon flaticon-clock-3"></span> 11 hours ago
+                  </li>
+                  <li>
+                    <span className="icon flaticon-money"></span> $35k - $45k
+                  </li>
+                </ul>
+                <ul className="job-other-info">
+                  <li className="time">Full Time</li>
+                  <li className="privacy">Private</li>
+                  <li className="required">Urgent</li>
+                </ul>
+                <button className="bookmark-btn">
+                  <span className="flaticon-bookmark"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="job-block col-lg-6 col-md-12 col-sm-12">
+            <div className="inner-box">
+              <div className="content">
+                <span className="company-logo">
+                  <img src="images/resource/company-logo/1-6.png" alt="" />
+                </span>
+                <h4>
+                  <a href="#">Software Engineer (Android), Libraries</a>
+                </h4>
+                <ul className="job-info">
+                  <li>
+                    <span className="icon flaticon-briefcase"></span> Segment
+                  </li>
+                  <li>
+                    <span className="icon flaticon-map-locator"></span> London, UK
+                  </li>
+                  <li>
+                    <span className="icon flaticon-clock-3"></span> 11 hours ago
+                  </li>
+                  <li>
+                    <span className="icon flaticon-money"></span> $35k - $45k
+                  </li>
+                </ul>
+                <ul className="job-other-info">
+                  <li className="time">Full Time</li>
+                  <li className="privacy">Private</li>
+                  <li className="required">Urgent</li>
+                </ul>
+                <button className="bookmark-btn">
+                  <span className="flaticon-bookmark"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="btn-box">
+          <a href="#" className="theme-btn btn-style-one bg-blue">
+            <span className="btn-title">Load More Listing</span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedJobs;
