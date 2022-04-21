3fee80a717ecdb35b617aef4d1379fac2cfc9794
kallyas <kallyasmedia@gmail.com>
2022-04-21 18:29:41 +0300

    ch: create home page

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import Banner from "../components/banner/Banner";
+import Layout from "../components/layout/Layout";
+
+const Homepage = () => {
+  return (
+    <Layout>
+      <Banner />
+    </Layout>
+  );
+};
+
+export default Homepage;
