60529c63e053a8a1dc67a6cf859f952988b5663b
kallyas <kallyasmedia@gmail.com>
2022-04-21 21:28:36 +0300

    ch: add about section

---
+++ frontend/src/components/about/AboutSection.js
@@ -0,0 +1,100 @@
+/* eslint-disable */
+
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <section className="about-section">
+      <div className="auto-container">
+        <div className="row">
+          <div className="content-column col-lg-6 col-md-12 col-sm-12 order-2">
+            <div
+              className="inner-column wow fadeInUp animated"
+              style={{ visibility: "visible", animationName: "fadeInUp" }}
+            >
+              <div className="sec-title">
+                <h2>Millions of Jobs. Find the one that suits you.</h2>
+                <div className="text">
+                  Search all the open positions on the web. Get your own personalized salary
+                  estimate. Read reviews on over 600,000 companies worldwide.
+                </div>
+              </div>
+              <ul className="list-style-one">
+                <li>Bring to the table win-win survival</li>
+                <li>Capitalize on low hanging fruit to identify</li>
+                <li>But I must explain to you how all this</li>
+              </ul>
+              <a href="#" className="theme-btn btn-style-one bg-blue">
+                <span className="btn-title">Get Started</span>
+              </a>
+            </div>
+          </div>
+          <div className="image-column col-lg-6 col-md-12 col-sm-12">
+            <figure
+              className="image wow fadeInLeft animated"
+              style={{ visibility: "visible", animationName: "fadeInLeft" }}
+            >
+              <img src="images/resource/image-2.jpg" alt="" />
+            </figure>
+            <div
+              className="count-employers wow fadeInUp animated"
+              style={{ visibility: "visible", animationName: "fadeInUp" }}
+            >
+              <div className="check-box">
+                <span className="flaticon-tick"></span>
+              </div>
+              <span className="title">300k+ Employers</span>
+              <figure className="image">
+                <img src="images/resource/multi-logo.png" alt="" />
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div className="fun-fact-section">
+          <div className="row">
+            <div
+              className="counter-column col-lg-4 col-md-4 col-sm-12 wow fadeInUp animated"
+              style={{ visibility: "visible", animationName: "fadeInUp" }}
+            >
+              <div className="count-box counted">
+                <span className="count-text" data-speed="3000" data-stop="4">
+                  4
+                </span>
+                M
+              </div>
+              <h4 className="counter-title">4 million daily active users</h4>
+            </div>
+            <div
+              className="counter-column col-lg-4 col-md-4 col-sm-12 wow fadeInUp animated"
+              data-wow-delay="400ms"
+              style={{ visibility: "visible", animationDelay: 400, animationName: "fadeInUp" }}
+            >
+              <div className="count-box counted">
+                <span className="count-text" data-speed="3000" data-stop="12">
+                  12
+                </span>
+                k
+              </div>
+              <h4 className="counter-title">Over 12k open job positions</h4>
+            </div>
+            <div
+              className="counter-column col-lg-4 col-md-4 col-sm-12 wow fadeInUp animated"
+              data-wow-delay="800ms"
+              style={{ visibility: "visible", animationDelay: 800, animationName: "fadeInUp" }}
+            >
+              <div className="count-box counted">
+                <span className="count-text" data-speed="3000" data-stop="20">
+                  20
+                </span>
+                M
+              </div>
+              <h4 className="counter-title">Over 20 million stories shared</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
