e460fa37c48d38e01ce8e172a7261a3fd0f57d0b
kallyas <kallyasmedia@gmail.com>
2022-04-21 21:30:31 +0300

    ch: add testimonials

---
+++ frontend/src/components/testimonials/Testimonials.js
@@ -0,0 +1,187 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <section className="testimonial-section">
+      <div className="container-fluid">
+        <div className="sec-title text-center">
+          <h2>Testimonials From Our Customers</h2>
+          <div className="text">Lorem ipsum dolor sit amet elit, sed do eiusmod tempor</div>
+        </div>
+
+        <div
+          className="carousel-outer wow fadeInUp animated"
+          style={{ visibility: "visible", animationName: "fadeInUp" }}
+        >
+          <div className="testimonial-carousel owl-carousel owl-theme owl-loaded owl-drag">
+            <div className="owl-stage-outer">
+              <div
+                className="owl-stage"
+                style={{
+                  transform: "translate3d(-1250px, 0px, 0px)",
+                  transition: "all 0.5s ease 0s",
+                  width: "4375px",
+                }}
+              >
+                <div className="owl-item cloned" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Great quality!</h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-2.png" alt="" />
+                        </div>
+                        <h4 className="name">Gabriel Nolan</h4>
+                        <span className="designation">Consultant</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="owl-item cloned" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Awesome Design </h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-3.png" alt="" />
+                        </div>
+                        <h4 className="name">Ashley Jenkins</h4>
+                        <span className="designation">Designer</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="owl-item active" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Good theme</h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-1.png" alt="" />
+                        </div>
+                        <h4 className="name">Nicole Wells</h4>
+                        <span className="designation">Web Developer</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="owl-item" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Great quality!</h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-2.png" alt="" />
+                        </div>
+                        <h4 className="name">Gabriel Nolan</h4>
+                        <span className="designation">Consultant</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="owl-item" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Awesome Design </h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-3.png" alt="" />
+                        </div>
+                        <h4 className="name">Ashley Jenkins</h4>
+                        <span className="designation">Designer</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="owl-item cloned" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Good theme</h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-1.png" alt="" />
+                        </div>
+                        <h4 className="name">Nicole Wells</h4>
+                        <span className="designation">Web Developer</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="owl-item cloned" style={{ width: "610px", marginRight: "15px" }}>
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      <h4 className="title">Great quality!</h4>
+                      <div className="text">
+                        Without JobHunt i’d be homeless, they found me a job and got me sorted out
+                        quickly with everything! Can’t quite… The Mitech team works really hard to
+                        ensure high level of quality
+                      </div>
+                      <div className="info-box">
+                        <div className="thumb">
+                          <img src="images/resource/testi-thumb-2.png" alt="" />
+                        </div>
+                        <h4 className="name">Gabriel Nolan</h4>
+                        <span className="designation">Consultant</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="owl-nav disabled">
+              <div className="owl-prev">
+                <span className="flaticon-back"></span>
+              </div>
+              <div className="owl-next">
+                <span className="flaticon-next"></span>
+              </div>
+            </div>
+            <div className="owl-dots">
+              <div className="owl-dot active">
+                <span></span>
+              </div>
+              <div className="owl-dot">
+                <span></span>
+              </div>
+              <div className="owl-dot">
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
