e35d3794251b4b549f4436f23e63828064ecf984
kallyas <kallyasmedia@gmail.com>
2022-04-21 18:30:00 +0300

    ch: add auth service

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,59 @@
+import api from "../api/api";
+
+const loginUser = async (user) => {
+  const res = await api.POST("/login", user);
+  const data = await res.json();
+  if (data.access_token) {
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    return data.user;
+  } else {
+    throw new Error(data.message);
+  }
+};
+
+const loginCompany = async (company) => {
+  const res = await api.POST("/company/login", company);
+  const data = await res.json();
+  if (data.access_token) {
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    return data.user;
+  } else {
+    throw new Error(data.message);
+  }
+};
+
+
+const registerUser = async (user) => {
+  const res = await api.POST("/register", user);
+  const data = await res.json();
+  if (data.access_token) {
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    return data.user;
+  } else {
+    throw new Error(data.message);
+  }
+};
+
+
+const registerCompany = async (company) => {
+  const res = await api.POST("/company/register", company);
+  const data = await res.json();
+  if (data.access_token) {
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+    return data.user;
+  } else {
+    throw new Error(data.message);
+  }
+};
+
+
+export default {
+    loginUser,
+    loginCompany,
+    registerUser,
+    registerCompany
+};
