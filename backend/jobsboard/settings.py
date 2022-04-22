42bf3cbc18399ecd5213015e780e5641e68120d3
kallyas <kallyasmedia@gmail.com>
2022-04-22 16:05:24 +0300

    fix: cross origin

--- backend/jobsboard/settings.py
+++ backend/jobsboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&5gbb!(yu0bye9t7ln+g%g7vqt346_cfx!=iml(fi^r4@!kax1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'api',
     'jobs',
     'authapp',
@@ -52,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
 ROOT_URLCONF = 'jobsboard.urls'
 
