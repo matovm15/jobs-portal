6a0a7f80d6bfa5a9a0341a94ac413854ca0899a9
kallyas <kallyasmedia@gmail.com>
2022-04-22 16:03:41 +0300

    fix: login and regester methods

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,59 +1,56 @@
 import api from "../api/api";
 
 const loginUser = async (user) => {
-  const res = await api.POST("/login", user);
+  const res = await api.LOGIN("/auth/login/", user);
   const data = await res.json();
   if (data.access_token) {
     localStorage.setItem("access_token", data.access_token);
     localStorage.setItem("user", JSON.stringify(data.user));
     return data.user;
   } else {
-    throw new Error(data.message);
+    throw new Error(data.non_field_errors[0]);
   }
 };
 
 const loginCompany = async (company) => {
-  const res = await api.POST("/company/login", company);
+  const res = await api.LOGIN("/auth/company/login/", company);
   const data = await res.json();
   if (data.access_token) {
     localStorage.setItem("access_token", data.access_token);
     localStorage.setItem("user", JSON.stringify(data.user));
     return data.user;
   } else {
-    throw new Error(data.message);
+    throw new Error(data.data.non_field_errors[0]);
   }
 };
-
 
 const registerUser = async (user) => {
-  const res = await api.POST("/register", user);
+  const res = await api.REGISTER("/auth/register/", user);
   const data = await res.json();
   if (data.access_token) {
     localStorage.setItem("access_token", data.access_token);
     localStorage.setItem("user", JSON.stringify(data.user));
     return data.user;
   } else {
-    throw new Error(data.message);
+    throw new Error(data.data.non_field_errors[0]);
   }
 };
-
 
 const registerCompany = async (company) => {
-  const res = await api.POST("/company/register", company);
+  const res = await api.REGISTER("/auth/company/register/", company);
   const data = await res.json();
   if (data.access_token) {
     localStorage.setItem("access_token", data.access_token);
     localStorage.setItem("user", JSON.stringify(data.user));
     return data.user;
   } else {
-    throw new Error(data.message);
+    throw new Error(data.data.non_field_errors[0]);
   }
 };
-
 
 export const authService = {
   loginUser,
   loginCompany,
   registerCompany,
-  registerUser
-}
+  registerUser,
+};
