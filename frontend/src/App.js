e5bc951f4c0d26a22263a8bc0d321db65a407bd9
kallyas <kallyasmedia@gmail.com>
2022-04-22 16:04:51 +0300

    ch: updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import PrivateRoute from './utils/PrivateRoute';
+import Dashboard from './pages/user/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route  path="/" element={<Homepage />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
